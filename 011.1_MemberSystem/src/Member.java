50b121b587cad2ae5fa4134d25f260742970e5fa
chingan <chingan@192.168.11.121>
2017-08-24 17:34:28 +0800

    Next step start from sort.

--- 011.1_MemberSystem/src/Member.java
+++ 011.1_MemberSystem/src/Member.java
@@ -4,6 +4,9 @@ public class Member {
 	private Birthday birthday;
 	private int num;
 	
+	public Member() {
+		this.birthday = new Birthday();
+	}
 	public Member(int num, String name, String iD, int y, int m, int d, String phone) {
 		this.name = "Name undefined";
 		
@@ -45,6 +48,10 @@ public class Member {
 			return false;
 	}
 	
+	public boolean setNum(String i) {
+		int iInt = Integer.parseInt(i);
+		return (setNum(iInt));
+	}
 	public boolean setName(String nameInput) {
 		if (nameInput != null) {
 			this.name = nameInput;
@@ -65,6 +72,14 @@ public class Member {
 		this.birthday.setBirthday(y, m, d);
 	}
 	
+	public void setBirthday(String input) {
+		String[] temp = input.split("/");
+		int y = Integer.parseInt(temp[0]);
+		int m = Integer.parseInt(temp[1]);
+		int d = Integer.parseInt(temp[2]);
+		this.setBirthday(y, m, d);
+	}
+	
 	private static String idCheck(String input) {
 		//Transform the first character into integer to check if it's character.
 		int a = (int)input.charAt(0);
@@ -74,7 +89,7 @@ public class Member {
 		
 		if (input.length() != 10)
 			return "A123456789";
-		else if ((int)(input.charAt(1)) != 1 || (int)(input.charAt(1)) != 2)
+		else if (Character.digit(input.charAt(1), 2 + 1) == -1)
 			return "A123456789";
 		//Check first character.
 		else if (a >= 65 && a <= 90) {
@@ -86,19 +101,21 @@ public class Member {
 				int[] numbers = new int[10];
 				numbers[0] = num1;
 				for (int i = 1; i < 10; i++) 
-					numbers[i] = charTrans(input.charAt(i));
+					numbers[i] = Character.digit(input.charAt(i), 10);
 				
-				int temp = num1;
+				int temp = num1, temptest;
 				for (int i = 1; i < 9; i++) {
 					temp += numbers[i] * (9 - i);
 				}
-				if (temp == numbers[9])
+				temp = 10 - (temp % 10); 
+				if (temp == 10 && numbers[9] == 0)
+					return input;
+				else if(temp == numbers[9])
 					return input;
 			}
 		}
 		return "A123456789";
 	}
-	
 	private static int charTrans(char first) {
 		int[] transInt = new int[27];
 
@@ -113,7 +130,7 @@ public class Member {
 		transInt[23] = 32;
 		transInt[24] = 30;
 		transInt[25] = 31;
-		transInt[25] = 32;
+		transInt[26] = 33;
 		
 		//Make table of number of each character.
 		for (int i = 1; i < 27; i++) 
@@ -135,9 +152,13 @@ public class Member {
 	@Override
 	public String toString() {
 		String output;
-		output = String.format("%03d", this.num) + "\t" + String.format("%3S",this.name) + "\t" + String.format("%10S",this.iD) + "\t" + this.birthday + " \t" + String.format("%10S",this.phone);	
+		output = String.format("%03d\t%3s\t%10s\t%s\t%10s", this.num, this.name, this.iD, this.birthday, this.phone);	
 		return output;
 	}
 	
+	public static String printTitle() {
+		String s = String.format("%s\t%s\t%s\t%s\t%10s", "編號", "姓名", "身分證字號", "生日", "電話");
+		return s;
+	}
 
 }
--- 011.1_MemberSystem/src/TestMemberSystem.java
+++ 011.1_MemberSystem/src/TestMemberSystem.java
@@ -1,91 +1,160 @@
 
 import java.io.*;
 import java.lang.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TestMemberSystem {
+	//Used to store member data, [0]will be dummy.
+	public static Member[] membersArray = new Member[50];
+	//Record number of string lines read from file.
+	public static int count = 0;
+	public static Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-//		Member[] membersArray = new Member[50];
-//		
-//		
-//		membersArray[0] = new Member(1,"黃獻鋒", "F227518590", 1988, 2, 3,"0225342312");
-//		membersArray[1] = new Member(5,"黃獻鋒", "A128099248", 1990, 3, 5,"0222312");
-//		for (int i = 0; i < 2; i++)
-//		System.out.println(membersArray[i]);
+	public static void main(String[] args) 
+		throws IOException, FileNotFoundException{
+		int status = 0, input; 
 		
-		String a = "F227518590";
-		String b = idCheck(a);
-		System.out.println(a);
-		System.out.println(b);
-
-
-	}
-	
-	private static String idCheck(String input) {
-		//Transform the first character into integer to check if it's character.
-		int a = (int)input.charAt(0);
-		//If first character is lower case, trans into upper case.
-		if (a >= 97 && a <= 122)
-			a -= 32;
-		
-		if (input.length() != 10)
-			return "A123456789";
-		else if ((int)(input.charAt(1)) != 1 || (int)(input.charAt(1)) != 2)
-			return "A123456789";
-		//Check first character.
-		else if (a >= 65 && a <= 90) {
-			int num1 = charTrans(input.charAt(0));
-			if (num1 == -1)
-				return "A123456789";
-			else {
-				//Check if id is legal.
-				int[] numbers = new int[10];
-				numbers[0] = num1;
-				for (int i = 1; i < 10; i++) 
-					numbers[i] = charTrans(input.charAt(i));
+		System.out.print("載入資料檔名：");
+		String fileName = scanner.next();
+		readFile(fileName);
+		while (status != -1) {
+			switch(status) {
+			//Input option and edit status.
+			case 0:
+				System.out.print("選項：1)檢視全部, 2)新增, 3)刪除, 4)修改, 5)篩選編號, -1) 結束? ");
+				input = scanner.nextInt();
+				if (input == -1)
+					status = -1;
+				else if (input > 0 && input < 6)
+					status = input;
+				else
+					System.out.println("Input error!");
+				break;
+			//Show all data.
+			case 1:
+				showAll();
+				status = 0;
+				break;
+			//Add new datum.
+			case 2:
 				
-				int temp = num1;
-				for (int i = 1; i < 9; i++) {
-					temp += numbers[i] * (9 - i);
-				}
-				if (temp == numbers[9])
-					return input;
+				break;
 			}
 		}
-		return "A123456789";
+		System.out.print("輸出資料檔名：");
+		fileName = scanner.next();
+		outPut(fileName);
+		System.out.println("Programe terminate.");
 	}
-	private static int charTrans(char first) {
-		int[] transInt = new int[27];
-
-		for (int i = 1; i < 9; i++) 
-			transInt[i] = 9 + i;
-		transInt[9] = 34;
-		for (int i = 10; i < 15; i++)
-			transInt[i] = i + 8;
-		transInt[15] = 35;
-		for (int i = 16; i < 23; i++)
-			transInt[i] = i + 7;
-		transInt[23] = 32;
-		transInt[24] = 30;
-		transInt[25] = 31;
-		transInt[25] = 32;
-		
-		//Make table of number of each character.
-		for (int i = 1; i < 27; i++) 
-			transInt[i] = transInt[i] / 10 + transInt[i] % 10 * 9;
-		
-		char[] alphabet = new char[27];
-		for (int i = 1; i < 27; i++) {
-			alphabet[i] = (char)(i + 64);
+	
+	public static Member readInToMember(String input) {
+		String[] storeStrings = input.split("\t");
+		Member returnMember = new Member();
+		returnMember.setNum(storeStrings[0]);
+		returnMember.setName(storeStrings[1]);
+		returnMember.setID(storeStrings[2]);
+		returnMember.setBirthday(storeStrings[3]);
+		returnMember.setPhone(storeStrings[4]);
+		return returnMember;
+	}
+	
+	public static void readFile(String fileName) 
+			throws IOException, FileNotFoundException{
+		String[] strsOfFileIn = new String[50];
+		//Create new object of BufferedReader to read file in.
+		BufferedReader fileIn = new BufferedReader(new FileReader(fileName));
+		String readStr;
+		//Read content of file in into string[] and record how many data are in this file.
+		while ((readStr = fileIn.readLine()) != null) {
+			strsOfFileIn[count++] = readStr;
 		}
-		
-		for (int i = 1; i < 27; i++) {
-			if (alphabet[i] == first)
-				return transInt[i];
+		fileIn.close();
+		//Transform read in strings into Member and stored into membersArray.
+		for (int i = 1; i < count; i++) {
+			//If number of data is more than array size, resize it.
+			if (i > membersArray.length)
+				membersArray = Arrays.copyOf(membersArray, count * 2);
+			membersArray[i] = readInToMember(strsOfFileIn[i]);
+			System.out.println(membersArray[i]);
 		}
+	}
+	
+	public static void outPut(String fileName) 
+			throws IOException, FileNotFoundException {
+		//Create an object to output file.
+		BufferedWriter fileOut = new BufferedWriter(new FileWriter(fileName));
+		fileOut.write(Member.printTitle());
+		for (int i = 1; i < count; i++) {
+			fileOut.newLine();
+			fileOut.write(membersArray[i].toString());
+		}
+		fileOut.flush();
+		fileOut.close();
+		System.out.println("檔案寫入成功！");
+	}
+	
+	public static void showAll() {
+		System.out.println(Member.printTitle());
+		for (int i = 0; i < 20; i++)
+			System.out.print("=");
+		System.out.println("");
+		for (int i = 0; i < count; i++)
+			System.out.println(membersArray[i]);
+		System.out.println("");
+	}
+	
+	public static void add() {
+		int input, num, y, m, d;
+		String name, iD, phone;
 		
+		while (true) {
+			System.out.print("編號(介於0-100)：");
+			input = scanner.nextInt();
+			if (input < 0 || input > 100)
+				System.out.println("錯誤！超出範圍！");
+			else if (search(input) != -1)
+				System.out.println("錯誤！編號重複！");
+			else {
+				num = input;
+				break;
+			}
+		}
+		while (true) {
+			System.out.print("編號(介於0-100)：");
+			input = scanner.nextInt();
+			if (input < 0 || input > 100)
+				System.out.println("Input error!");
+			else {
+				num = input;
+				break;
+			}
+		}
+		System.out.print("姓名：");
+		name = scanner.next();
+		System.out.print("身分證字號：");
+		iD = scanner.next();
+		System.out.print("生日（年/月/日）：");
+		y = scanner.nextInt();
+		m = scanner.nextInt();
+		d = scanner.nextInt();
+		System.out.print("電話：");
+		phone = scanner.next();
+		membersArray[count++] = new Member(num, name, iD, y, m, d, phone);
+		System.out.println("會員新增成功!");
+		
+	}
+	
+	public static int search(int target) {
+		for (int i = 0; i < count; i++) {
+			if (membersArray[i].getNum() == target)
+				return i;
+		}
 		return -1;
+	}
+	
+	public static void sort() {
+		
 	}
 	
 }
